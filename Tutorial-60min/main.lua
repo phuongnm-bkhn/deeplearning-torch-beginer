35c09e425f4c5fab4ede3c3896fa345328997ce0
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2016-12-19 02:13:17 +0700

    - Update full code test.

--- Tutorial-60min/main.lua
+++ Tutorial-60min/main.lua
@@ -1,5 +1,8 @@
 require 'paths'
+require 'nn'
+require 'rnn'
 
+-- 1 setup data 
 if (not paths.filep("cifar10torchsmall.zip")) then
     os.execute('wget -c https://s3.amazonaws.com/torch7/data/cifar10torchsmall.zip')
     os.execute('unzip cifar10torchsmall.zip')
@@ -13,11 +16,8 @@ testset = torch.load('cifar10-test.t7')
 classes = {'airplane', 'automobile', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
 
-print(trainset)
-print(#trainset.data)
-
-print(#trainset.data[100]) -- display the 100-th image in dataset
-print(classes[trainset.label[100]])
+print('struct data trainset: ', trainset)
+print('label element at 100: ',classes[trainset.label[100]])
 
 -- 
 -- pre process object trainset
@@ -33,10 +33,69 @@ function trainset:size()
     return self.data:size(1) 
 end
 
--- test object trainset 
-print(trainset:size()) -- just to test
-print(trainset[33]) -- load sample number 33.
-print(#trainset[33][1])
 
-redChannel = trainset.data[{ {}, {1}, {}, {}  }] -- this picks {all images, 1st channel, all vertical pixels, all horizontal pixels}
-print(#redChannel)
+mean = {} -- store the mean, to normalize the test set in the future
+stdv  = {} -- store the standard-deviation for the future
+for i=1,3 do -- over each image channel
+    mean[i] = trainset.data[{ {}, {i}, {}, {}  }]:mean() -- mean estimation
+    print('Channel ' .. i .. ', Mean: ' .. mean[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+    
+    stdv[i] = trainset.data[{ {}, {i}, {}, {}  }]:std() -- std estimation
+    print('Channel ' .. i .. ', Standard Deviation: ' .. stdv[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+-- 
+-- 2 setup noron net
+net = nn.Sequential()
+net:add(nn.SpatialConvolution(3, 6, 5, 5)) -- 3 input image channels, 6 output channels, 5x5 convolution kernel
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))     -- A max-pooling operation that looks at 2x2 windows and finds the max.
+net:add(nn.SpatialConvolution(6, 16, 5, 5))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))
+net:add(nn.View(16*5*5))                    -- reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
+net:add(nn.Linear(16*5*5, 120))             -- fully connected layer (matrix multiplication between input and weights)
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(120, 84))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of the network (in this case, 10 digits)
+net:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classification problems
+
+-- 3 define loss function 
+criterion = nn.ClassNLLCriterion()
+
+-- 4 train 
+maxIter = 5
+trainer = nn.StochasticGradient(net, criterion)
+trainer.learningRate = 0.001
+trainer.maxIteration = maxIter -- just do 5 epochs of training.
+trainer:train(trainset)
+
+-- 5 test 
+testset.data = testset.data:double()   -- convert from Byte tensor to Double tensor
+for i=1,3 do -- over each image channel
+    testset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction    
+    testset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+torch.save('model_' .. maxIter .. '.t7', net)
+
+correct = 0
+class_performance = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+for i=1,10000 do
+    local groundtruth = testset.label[i]
+    local prediction = net:forward(testset.data[i])
+    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+    if groundtruth == indices[1] then
+        correct = correct + 1
+        class_performance[groundtruth] = class_performance[groundtruth] + 1
+    end
+end
+print ('ti le chinh xac tong hop')
+print(correct, 100*correct/10000 .. ' % ')
+print ('ti le chinh xac tren tung nhan ')
+for i=1,#classes do
+    print(classes[i], 100*class_performance[i]/1000 .. ' %')
+end
